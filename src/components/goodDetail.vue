b7e6bb8632c101bdf389d0746c393a5dcd713cfa
allan que <1132799638@qq.com>
2018-10-24 18:07:00 +0800

    detail

--- src/components/goodDetail.vue
+++ src/components/goodDetail.vue
@@ -10,13 +10,51 @@
       </router-link>
     </mt-header >
     <div class="xiangqingtu">
-      <mt-swipe :auto="4000">
-        <mt-swipe-item>1</mt-swipe-item>
-        <mt-swipe-item>2</mt-swipe-item>
-        <mt-swipe-item>3</mt-swipe-item>
-        <mt-swipe-item>4</mt-swipe-item>
+      <mt-swipe :auto="8000">
+        <mt-swipe-item v-for="(item,idx) in swipepic" :key="idx" >
+          <img :src="item.imgurl" />
+        </mt-swipe-item>
       </mt-swipe>
     </div>
+    <!-- 间隙 -->
+    <div class="jianxi"></div>
+    <!-- 手机详细信息 -->
+    <div class="details">
+      <div class="detail-title">
+        <span>红魔竞技游戏手机</span>
+        <span>曜石黑</span>
+        <span>6GB+64GB</span>
+        <span class="last-icon"> 
+          <font-awesome-icon icon="share-alt" />
+        </span>
+      </div>
+      <div class="detail-jiangjia">
+      <span style="color: rgb(255, 77, 77); font-size: 18px; font-weight: 800;">10.20日上午10:00【6GB+64GB 直降200】</span>
+      </div>
+      <div class="jiaqian">
+        <div class="pdt-price" style="color: rgb(255, 77, 77); display: inline-block;">￥2299.00元</div>
+        <del class="pdt-orig-price" style="color: rgb(153, 153, 153);">￥2499.00元</del>
+      </div>
+      <div class="cuxiao">
+        <p>促销</p>
+        <!-- 间隙   &  -->
+        <div class="jianxi-zeng"></div>
+         <!-- 赠牛豆 -->
+         <div>
+           <span class="zeng">赠牛豆</span><span> 购买完成可获得与支付金额相同数目的牛豆</span><br>
+           <span class="zeng">包邮</span><span>  普通和青铜会员满59、白银满39、黄金以上包邮</span>
+         </div>
+      </div>
+      
+    
+
+    
+     
+
+      
+    </div>
+
+
   </div>
 </template>
 <script>
@@ -24,6 +62,12 @@ import '../style/sass/goodDetail.scss'
 export default {
   data(){
       return {
+        swipepic:[
+          {imgurl: "./static/img/category/shouji1.png"},
+          {imgurl: "./static/img/category/shouji2.png"},
+          {imgurl: "./static/img/category/shouji3.png"},
+          {imgurl: "./static/img/category/shouji4.png"}
+        ],
         id: this.$route.query.goodId,
         loading: false,
         selectedGood: []
--- src/plugins/components.js
+++ src/plugins/components.js
@@ -3,10 +3,10 @@ import {Button, Tabbar, TabItem, Header, Cell, Swipe, SwipeItem} from 'mint-ui'
 
 // 字体图标
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHome, faList, faShoppingCart, faUser, faChevronLeft, faEquals } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faList, faShoppingCart, faUser, faChevronLeft, faEquals, faShareAlt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 // 添加具体的字体图标
-library.add(faHome, faList, faShoppingCart, faUser, faChevronLeft, faEquals)
+library.add(faHome, faList, faShoppingCart, faUser, faChevronLeft, faEquals, faShareAlt)
 export default (Vue) => {
   Vue.component('font-awesome-icon', FontAwesomeIcon)
   Vue.component(Button.name, Button)
